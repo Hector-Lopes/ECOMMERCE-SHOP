97e085b5f163ed20c60c06572b06dc810c38d81b
Hector-Lopes <hector27.lopes@gmail.com>
2024-07-31 14:00:32 -0300

    refactor: change the path of files

---
+++ src/components/header/header.component.tsx
@@ -0,0 +1,28 @@
+import { BsCart3 } from 'react-icons/bs'
+
+import {
+  HeaderContainer,
+  HeaderItem,
+  HeaderItems,
+  HeaderTitle
+} from './header.styles'
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <HeaderTitle>CLUB CLOTHING</HeaderTitle>
+
+      <HeaderItems>
+        <HeaderItem>Explorar</HeaderItem>
+        <HeaderItem>Login</HeaderItem>
+        <HeaderItem>Criar Conta</HeaderItem>
+        <HeaderItem>
+          <BsCart3 size={25} />
+          <p style={{ marginLeft: 5 }}>5</p>
+        </HeaderItem>
+      </HeaderItems>
+    </HeaderContainer>
+  )
+}
+
+export default Header
