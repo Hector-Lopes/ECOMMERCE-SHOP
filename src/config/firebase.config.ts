f78eee2ecee0a688a87289eb5f6297ca193eb648
hector.lopes <hector27.lopes@gmail.com>
2024-08-09 09:30:18 -0300

    chore: add firebase config and firestore

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDsjlpE2kxB3xhNhYOLTQ7qgB7HskokkBg',
+  authDomain: 'club-ecommerce-2-5a7b3.firebaseapp.com',
+  projectId: 'club-ecommerce-2-5a7b3',
+  storageBucket: 'club-ecommerce-2-5a7b3.appspot.com',
+  messagingSenderId: '317956906636',
+  appId: '1:317956906636:web:c02f052ced451d5f41a5a9'
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
