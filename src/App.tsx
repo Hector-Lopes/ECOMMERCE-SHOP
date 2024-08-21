aca1c9195de9e1f227514fa5f84b2ad4dcf0e3d4
hector.lopes <hector27.lopes@gmail.com>
2024-08-21 16:09:27 -0300

    feat: use context state to verify the auth state in all app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useContext } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 //components
 import Home from './pages/home/home.page'
 import LoginPage from './pages/login/login.page'
 import SignUpPage from './pages/sign-up/sign-up.page'
 import { onAuthStateChanged } from 'firebase/auth'
-import { auth } from './config/firebase.config'
-
+import { auth, db } from './config/firebase.config'
+import { UserContext } from './contexts/user.context'
+import { collection, getDocs, query, where } from 'firebase/firestore'
+import User from '../src/types/user'
 const App = () => {
-  onAuthStateChanged(auth, (user) => {
-    console.log(user)
+  const { loginUser, isAuthenticated, logoutUser } = useContext(UserContext)
+  onAuthStateChanged(auth, async (user) => {
+    const isSigningOut = isAuthenticated && !user
+    if (isSigningOut) {
+      return logoutUser()
+    }
+
+    const isSigninIn = !isAuthenticated && user
+    if (isSigninIn) {
+      const querySnapshot = await getDocs(
+        query(collection(db, 'users'), where('id', '==', user.uid))
+      )
+      const userFromFirestore = querySnapshot.docs[0]?.data()
+      return loginUser(userFromFirestore as User)
+    }
   })
   return (
     <>
