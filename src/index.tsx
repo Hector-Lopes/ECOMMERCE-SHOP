cac254d7fbf6d5340f89a35cb921f95644531c84
hector.lopes <hector27.lopes@gmail.com>
2024-08-22 15:07:59 -0300

    feat: add category context with loading verify

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import UserContextProvider from './contexts/user.context'
+import CategoryContextProvider from './contexts/catergory.context'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <UserContextProvider>
-      <App />
+      <CategoryContextProvider>
+        <App />
+      </CategoryContextProvider>
     </UserContextProvider>
   </React.StrictMode>
 )
