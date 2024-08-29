211a7eb116c046c7e8927d53d580b4772d0fdb14
hector.lopes <hector27.lopes@gmail.com>
2024-08-29 16:37:22 -0300

    feat: add map to list all products in cart

--- src/components/cart/cart-component.tsx
+++ src/components/cart/cart-component.tsx
@@ -10,15 +10,18 @@ import {
 } from './cart.styles'
 import CustomButton from '../custom-button/custom-button'
 import { CartContext } from '../../contexts/cart.context'
+import CartItem from '../cart-item.styles.ts/cart-item.component'
 
 const Cart: FunctionComponent = () => {
-  const { isVisible, toggleCart } = useContext(CartContext)
+  const { isVisible, toggleCart, products } = useContext(CartContext)
   return (
     <CartContainer isVisible={isVisible}>
       <CartEscapeArea onClick={toggleCart}></CartEscapeArea>
       <CartContent>
         <CartTitle>Seu Carrinho</CartTitle>
-        {/* produtos */}
+        {products.map((product) => (
+          <CartItem key={product.id} product={product}></CartItem>
+        ))}
         <CartTotal>Total: R$999</CartTotal>
         <CustomButton startIcon={<BsCartCheck></BsCartCheck>}>
           Ir para o Checkout
