652e75dfd128456a4dbdc0e55200cd3849c1ce1a
hector.lopes <hector27.lopes@gmail.com>
2024-08-29 16:36:49 -0300

    feat: add button-add to product

--- src/components/product-item/product-item-component.tsx
+++ src/components/product-item/product-item-component.tsx
@@ -1,19 +1,34 @@
 import Product from '../../types/products.types'
-
+import CustomButton from '../custom-button/custom-button'
+import { BsCartPlus } from 'react-icons/bs'
 import {
   ProductContainer,
   ProductImage,
   ProductInfo
 } from './product-item.styles'
+import { useContext } from 'react'
+import { CartContext } from '../../contexts/cart.context'
 
 interface ProductItemProps {
   product: Product
 }
 
 const ProductItem: React.FC<ProductItemProps> = ({ product }) => {
+  const { addProductToCart } = useContext(CartContext)
+
+  const HandleAddToCart = () => {
+    addProductToCart(product)
+  }
   return (
     <ProductContainer>
-      <ProductImage imageUrl={product.imagaurl}></ProductImage>
+      <ProductImage imageUrl={product.imageUrl}>
+        <CustomButton
+          onClick={HandleAddToCart}
+          startIcon={<BsCartPlus></BsCartPlus>}
+        >
+          Adicionar o carrinho
+        </CustomButton>
+      </ProductImage>
       <ProductInfo>
         <p>{product.name}</p>
         <p>{product.price}</p>
