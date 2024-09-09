96cfe5a09f6a91b7160585f14d6a443437dda43e
hector.lopes <hector27.lopes@gmail.com>
2024-09-09 09:18:55 -0300

    refct: remove userContext

--- src/index.tsx
+++ src/index.tsx
@@ -13,13 +13,11 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <UserContextProvider>
-        <CategoryContextProvider>
-          <CartContextProvider>
-            <App />
-          </CartContextProvider>
-        </CategoryContextProvider>
-      </UserContextProvider>
+      <CategoryContextProvider>
+        <CartContextProvider>
+          <App />
+        </CartContextProvider>
+      </CategoryContextProvider>
     </Provider>
   </React.StrictMode>
 )
