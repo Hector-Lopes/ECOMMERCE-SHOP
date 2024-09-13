c699390a1bded969c1f047ea3a681bb2765d2582
hector.lopes <hector27.lopes@gmail.com>
2024-09-13 11:50:21 -0300

    feat: add redux persist to keep the cart state if the page reloading

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,23 @@
 import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import rootReducer from './root.reducer'
+//@ts-ignore
+import storage from 'redux-persist/lib/storage'
+//@ts-ignore
+import persistReducer from 'redux-persist/es/persistReducer'
+//@ts-ignore
+import persistStore from 'redux-persist/es/persistStore'
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cartReducer']
+}
 
-const store = createStore(rootReducer, applyMiddleware(logger))
+const persistedRootReducer: typeof rootReducer = persistReducer(
+  persistConfig,
+  rootReducer
+)
 
+export const store = createStore(persistedRootReducer, applyMiddleware(logger))
+export const persistedStore = persistStore(store)
 export type RootState = ReturnType<typeof store.getState>
-
-export default store
