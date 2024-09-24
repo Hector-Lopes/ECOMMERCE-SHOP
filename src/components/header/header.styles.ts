0e9e167800dc3eb7f885f448f04f1d7c69599822
hector.lopes <hector27.lopes@gmail.com>
2024-09-24 09:31:22 -0300

    style: add responsiveness to Header component

--- src/components/header/header.styles.ts
+++ src/components/header/header.styles.ts
@@ -8,18 +8,36 @@ export const HeaderContainer = styled.div`
   justify-content: space-between;
   padding: 20px;
   color: ${Colors.text.white};
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 `
 
 export const HeaderTitle = styled.h2`
   font-weight: bold;
   font-size: 1.5rem;
-  cursor: pointer;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  @media (max-width: 768px) {
+    margin-bottom: 16px;
+  }
 `
 
 export const HeaderItems = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    justify-content: space-evenly;
+  }
 `
 
 export const HeaderItem = styled.div`
@@ -32,6 +50,10 @@ export const HeaderItem = styled.div`
   &:nth-child(2),
   &:nth-child(3) {
     margin-right: 40px;
+
+    @media (max-width: 768px) {
+      margin-right: 0;
+    }
   }
 
   &:hover {
