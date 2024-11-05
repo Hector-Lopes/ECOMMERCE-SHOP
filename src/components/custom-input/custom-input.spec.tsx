3c54e88f425b81635b36abd45bfcf221aea082e5
Hector-Lopes <hector27.lopes@gmail.com>
2024-11-05 11:50:13 -0300

    test: ensure the user type in the input

--- src/components/custom-input/custom-input.spec.tsx
+++ src/components/custom-input/custom-input.spec.tsx
@@ -1,15 +1,37 @@
 import { getByPlaceholderText, render } from "@testing-library/react"
 import CustomInput from "./custom-input.component"
 import colors from "../../theme/theme.colors"
+import userEvent from "@testing-library/user-event"
 
 describe('Custom Input',()=>{
-    it('should render with error if hasError is true',()=>{
+    it('should render with error style if hasError is true',()=>{
         const {getByPlaceholderText} = render(<CustomInput placeholder="Lorem Ipsum" hasError={true}></CustomInput>)
         const input = getByPlaceholderText('Lorem Ipsum')
    
    expect(input).toHaveStyle({border:`2px solid ${colors.error}` })
     })
     
+    it('should render without error style if hasError is false',()=>{
+        const {getByPlaceholderText} = render(<CustomInput placeholder="Lorem Ipsum" hasError={false}></CustomInput>)
+        const input = getByPlaceholderText('Lorem Ipsum')
+   
+   expect(input).toHaveStyle({border:`none` })
+    })
+    
+    it('should change value when user types',()=>{
+        const {getByPlaceholderText,getByDisplayValue} = render(
+            <CustomInput placeholder="Lorem ipsum" hasError={false}></CustomInput>
+        )
+      const  input = getByPlaceholderText('Lorem ipsum')
+     
+      userEvent.type(input,'Dolor Sit')
+
+      getByDisplayValue('Dolor Sit')
+    
+    })
+
+
+
 })
 
 
